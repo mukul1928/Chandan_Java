2c62633a3d58f3fa20a55752ff53f639ec83a54f
Chandan Pandey <chandanpandey1928@gmail.com>
2025-05-16 13:13:35 +0530

    keyword in java how to find out so the pink color will say it is a keyword 99% of the time

---
+++ src/class_3_Keywords/Keywords_In_JavaSelenium.java
@@ -0,0 +1,10 @@
+package class_3_Keywords;
+
+//here everything what we see in pink colour is a keywords
+public class Keywords_In_JavaSelenium {
+	public static void main(String[] args) {
+		boolean a = true;// not every colour in pink is a keyword in java so here this is not a keyword
+
+	}
+
+}
