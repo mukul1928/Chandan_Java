0e41cc9e4bd0f517e925db5fec3af95c8047176f
Chandan Pandey <chandanpandey1928@gmail.com>
2025-05-17 11:30:27 +0530

    Update Keywords_In_JavaSelenium.java

--- src/class_3_Keywords/Keywords_In_JavaSelenium.java
+++ src/class_3_Keywords/Keywords_In_JavaSelenium.java
@@ -1,6 +1,6 @@
 package class_3_Keywords;
 
-//here everything what we see in pink colour is a keywords
+//here everything what we see in pink colour is a keywords there are 50 keywords that we have in java
 public class Keywords_In_JavaSelenium {
 	public static void main(String[] args) {
 		boolean a = true;// not every colour in pink is a keyword in java so here true is not a keyword
