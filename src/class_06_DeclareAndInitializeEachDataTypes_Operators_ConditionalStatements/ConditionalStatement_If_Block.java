6929fd2da6206041529091b456780a2158bcbed2
Chandan Pandey <chandanpandey1928@gmail.com>
2025-05-19 15:15:51 +0530

    if block with false condition

--- src/class_06_DeclareAndInitializeEachDataTypes_Operators_ConditionalStatements/ConditionalStatement_If_Block.java
+++ src/class_06_DeclareAndInitializeEachDataTypes_Operators_ConditionalStatements/ConditionalStatement_If_Block.java
@@ -4,7 +4,8 @@ public class ConditionalStatement_If_Block {
 	public static void main(String[] args) {
 		int a = 100;
 		int b = 200;
-		if (b > a) {
+		if (b > a) // here the condition is true so the if block will execute 
+		{
 			int sum = b + a;
 			System.out.println(sum);
 		}
---
+++ src/class_06_DeclareAndInitializeEachDataTypes_Operators_ConditionalStatements/ConditionalStatement_If_Block_WithFalse.java
@@ -0,0 +1,15 @@
+package class_06_DeclareAndInitializeEachDataTypes_Operators_ConditionalStatements;
+
+public class ConditionalStatement_If_Block_WithFalse {
+	public static void main(String[] args) {
+		int a = 100;
+		int b = 200;
+		System.out.println("Shiva");
+		if (a > b)// here the condition is false so the if block will not execute but the Optional will execute
+		{
+			System.out.println("Chandan");
+		}
+		System.out.println("Optional");
+	}
+
+}
