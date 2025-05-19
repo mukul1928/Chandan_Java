3dd5f89794961bea59bb0c6f25145f35a66a62da
Chandan Pandey <chandanpandey1928@gmail.com>
2025-05-19 14:59:44 +0530

    class 6 arithmetic operators

---
+++ src/Assignments_Java_Classes/class_06_AirthmeticOperators.java
@@ -0,0 +1,47 @@
+package Assignments_Java_Classes;
+
+public class class_06_AirthmeticOperators {
+	static void add() {
+		int a = 290;
+		int b = 23444;
+		int sum = a + b;
+		System.out.println(sum);
+	}
+
+	static void sub() {
+		int a = 523434;
+		int b = 434343;
+		int subs = a - b;
+		System.out.println(subs);
+	}
+
+	static void mul() {
+		int a = 34343;
+		int b = 7867;
+		int mult = a * b;
+		System.out.println(mult);
+	}
+
+	static void div() {
+		int a = 66544666;
+		int b = 20;
+		int divi = a / b;
+		System.out.println(divi);
+	}
+
+	static void modu() {
+		int a = 33434;
+		int b = 223;
+		int modul = a % b;
+		System.out.println(modul);
+	}
+
+	public static void main(String[] args) {
+		add();
+		sub();
+		mul();
+		div();
+		modu();
+	}
+
+}
