a7347aaaaa371aced3b7259f40b0664b1a42223a
Chandan Pandey <chandanpandey1928@gmail.com>
2025-05-19 11:04:30 +0530

    Calling Static Methods Inside Main Method

--- src/class_3_Keywords/Third_Class.java
+++ src/class_3_Keywords/Third_Class.java
@@ -1,9 +1,11 @@
-package class_3_Keywords;
+package class_3_Keywords;//keywords are package,public,class,static,void
 
-public class Third_Class {
+public class Third_Class//Identifier 
+{
 	public static void main(String[] args)//main method 
 	{
-		System.out.println("India");
+		System.out.println("India");//seprators India is Literarls
+		System.out.println("Nepal");
 	}
 
 }
---
+++ src/class_5_Calling_Static_Method_Inside_Main_Method/Create_StaticMethod_Call_In_MainMethod.java
@@ -0,0 +1,14 @@
+package class_5_Calling_Static_Method_Inside_Main_Method;
+
+public class Create_StaticMethod_Call_In_MainMethod {
+	static void myname() {
+		System.out.println("My name is Chandan Pandey");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Starting of the main method");
+		myname();
+		System.out.println("Ending of the main method");
+	}
+
+}
