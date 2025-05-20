cd62173e732f2e22913ca90dd22ad16af150e167
Chandan Pandey <chandanpandey1928@gmail.com>
2025-05-20 10:34:39 +0530

    if else if

---
+++ src/class_07_MultipleIfBlocks_ConditionalStatements/ConditionalStatement_If_Else_If.java
@@ -0,0 +1,20 @@
+package class_07_MultipleIfBlocks_ConditionalStatements;
+
+public class ConditionalStatement_If_Else_If {
+	public static void main(String[] args) {
+		int a = 100;
+		int b = 200;
+		if(a>b) {
+			System.out.println("1");
+		}
+		else if(a!=b) {
+			System.out.println("2");
+		}
+		else if(a<=b) {
+			System.out.println("3");
+		}
+		else {
+			System.out.println("4");
+		}
+	}
+}
