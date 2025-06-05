d17890ffdd01fb850dcb181832a6f6b47244675b
Chandan Pandey <chandanpandey1928@gmail.com>
2025-06-05 17:18:43 +0530

    Throw and Throws

---
+++ src/class_31_Exceptional_Handling_Arrays_FinallyBlock_ThrowsAndThrow/ThrowKeyword.java
@@ -0,0 +1,11 @@
+package class_31_Exceptional_Handling_Arrays_FinallyBlock_ThrowsAndThrow;
+
+public class ThrowKeyword 
+{
+	public static void main(String[] args) 
+	{
+		//throw new NullPointerException();
+		//throw new NullPointerException("Hey Element is not holding any values");
+		throw new ArrayIndexOutOfBoundsException();
+	}
+}
