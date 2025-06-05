c640e5efa9d02d350dac5f10a14c5dc3136384ff
Chandan Pandey <chandanpandey1928@gmail.com>
2025-06-05 13:16:31 +0530

    try and catch

--- src/class_30_Exceptional_Handling_Try_And_Catch_Block/Exeptional_Handling.java
+++ src/class_30_Exceptional_Handling_Try_And_Catch_Block/Exeptional_Handling.java
@@ -3,16 +3,23 @@ package class_30_Exceptional_Handling_Try_And_Catch_Block;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Exeptional_Handling {
-	public static void main(String[] args) {
-		try {
+public class Exeptional_Handling
+{
+	public static void main(String[] args) 
+	{
+		try 
+		{
 			Scanner s1 = new Scanner(System.in);
 			int a = s1.nextInt(); //suppose 1st time i will enter 1
 			int c = 1 / a; //suppose 2nd time i will enter 0
 			System.out.println(c); //suppose 3rd time i will enter my name chandan
-		} catch (ArithmeticException a) {
+		} 
+		catch (ArithmeticException a) 
+		{
 			System.out.println("hey ! I handled the exception... ! execution continue 1");
-		} catch (InputMismatchException e) {
+		} 
+		catch (InputMismatchException e) 
+		{
 			System.out.println("hey ! I handled the exception... ! execution continue 2");
 		}
 	}
