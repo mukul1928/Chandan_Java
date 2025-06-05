f6c5b95659049e71586d3f3fd434d8425c30dd2f
Chandan Pandey <chandanpandey1928@gmail.com>
2025-06-05 22:19:56 +0530

    c

--- src/class_31_Exceptional_Handling_Arrays_FinallyBlock_ThrowsAndThrow/ThrowKeyword.java
+++ src/class_31_Exceptional_Handling_Arrays_FinallyBlock_ThrowsAndThrow/ThrowKeyword.java
@@ -1,11 +1,14 @@
 package class_31_Exceptional_Handling_Arrays_FinallyBlock_ThrowsAndThrow;
 
+import java.util.InputMismatchException;
+
 public class ThrowKeyword 
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws ArrayIndexOutOfBoundsException,NullPointerException,InputMismatchException
 	{
 		//throw new NullPointerException();
 		//throw new NullPointerException("Hey Element is not holding any values");
-		throw new ArrayIndexOutOfBoundsException();
+		//throw new ArrayIndexOutOfBoundsException();
+		throw new InputMismatchException();
 	}
 }
