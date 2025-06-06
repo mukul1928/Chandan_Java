2148b9923fa495c3ce0ed267c6deb2066d28dc1e
Chandan Pandey <chandanpandey1928@gmail.com>
2025-06-06 10:22:02 +0530

    Create SwitchCase.java

---
+++ src/class_32_SwitchCase_ThreadSleep_MathClass_AssertKeyword/SwitchCase.java
@@ -0,0 +1,28 @@
+package class_32_SwitchCase_ThreadSleep_MathClass_AssertKeyword;
+
+public class SwitchCase 
+{
+	public static void main(String[] args) 
+	{
+		switch(10)
+		{
+		case 1:
+			
+				System.out.println("Your logic 1 will be here");
+				break; // it will break your execution
+			
+		case 2:
+			
+				System.out.println("Your logic 2 will be here");
+				break;
+			
+		case 3:
+			
+				System.out.println("Your logic 3 will be here");
+			
+		case 4:
+			
+				System.out.println("Your logic 4 will be here");
+		}
+	}
+}
