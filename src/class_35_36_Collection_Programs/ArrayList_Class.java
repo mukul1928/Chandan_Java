32d20b73bb2b6eaf9f36260b63420171cc04198e
Chandan Pandey <chandanpandey1928@gmail.com>
2025-06-08 14:35:31 +0530

    ArrayList

---
+++ src/class_35_36_Collection_Programs/ArrayList_Class.java
@@ -0,0 +1,19 @@
+package class_35_36_Collection_Programs;
+
+import java.util.ArrayList;
+
+public class ArrayList_Class 
+{
+	public static void main(String[] args)
+	{
+		ArrayList a1 = new ArrayList();
+		a1.add(100);
+		a1.add(987);
+		a1.add(876);
+		a1.add("Chandan");
+		a1.add(false);
+		a1.add('M');
+		a1.add(3.345);
+		System.out.println(a1);
+	}
+}
