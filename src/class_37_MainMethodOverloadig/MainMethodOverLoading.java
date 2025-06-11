ba2c1daa0bf1ff4ff7502ef6bbd03b603ff96b0d
Chandan Pandey <chandanpandey1928@gmail.com>
2025-06-11 20:09:09 +0530

    Update MainMethodOverLoading.java

--- src/class_37_MainMethodOverloadig/MainMethodOverLoading.java
+++ src/class_37_MainMethodOverloadig/MainMethodOverLoading.java
@@ -2,16 +2,24 @@ package class_37_MainMethodOverloadig;
 
 public class MainMethodOverLoading 
 {
+	void main(double a)
+	{
+		System.out.println("double value 3.14");
+	}
+	
 	public static void main(String[] args) 
 	{
 		main();
 		main(100,745);
+		MainMethodOverLoading m = new MainMethodOverLoading();
+		m.main(1.2334343);
 	}
 	
 	public static void main() 
 	{
 		System.out.println("Main Method without parameters");
 	}
+	
 	public static void main(int a, int b)
 	{
 		System.out.println("Main method with int a and int b");
