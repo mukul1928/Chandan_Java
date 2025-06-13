1caa544eefe03b077a110c16fcda499c9d98c0a3
Chandan Pandey <chandanpandey1928@gmail.com>
2025-06-13 17:11:09 +0530

    Java Interview Questions

---
+++ src/Practice_Java_Interview_Questions/ReverseStringUsingStringBuffer.java
@@ -0,0 +1,8 @@
+package Practice_Java_Interview_Questions;
+
+public class ReverseStringUsingStringBuffer {
+	public static void main(String[] args) {
+		StringBuffer bf = new StringBuffer("Chandan Pandey");
+		System.out.println(bf.reverse());
+	}
+}
