781773a0c79ba88d647f915f4c6390765ace1e74
Chandan Pandey <chandanpandey1928@gmail.com>
2025-06-16 21:30:39 +0530

    Update Reverse_String_Then_Words_April_4_4_25.java

--- src/Practice_Java_Interview_Questions/Reverse_String_Then_Words_April_4_4_25.java
+++ src/Practice_Java_Interview_Questions/Reverse_String_Then_Words_April_4_4_25.java
@@ -6,14 +6,14 @@ public class Reverse_String_Then_Words_April_4_4_25 {
 		String name = "Welcome to the java class";
 		String reverse = "";
 
-		for (int i = name.length() - 1; i >= 0; i--) {
+		for (int i = name.length() - 1; i >= 0; i--) 
+		{
 			char answer = name.charAt(i);
 			reverse = reverse + answer;
-
 		}
-		System.out.println(reverse);
-		boolean value = name.equals(reverse);
-		System.out.println(value);
+			System.out.println(reverse);
+			boolean value = name.equals(reverse);
+			System.out.println(value);
 	}
 
 }
