9f25f4ff9757640bc6c6f543069c5a7b46770ef5
Chandan Pandey <chandanpandey1928@gmail.com>
2025-06-30 08:47:55 +0530

    Create CreateArraysAndFindSmallestValue.java

---
+++ src/Practice_Java_Interview_Questions/CreateArraysAndFindSmallestValue.java
@@ -0,0 +1,19 @@
+package Practice_Java_Interview_Questions;
+
+import java.util.Arrays;
+
+public class CreateArraysAndFindSmallestValue 
+{
+	public static void main(String[] args) 
+	{
+		int arr[] = {23,444,4,76,5};
+	//	Arrays.sort(arr);
+	//	System.out.println(Arrays.toString(arr));
+		for(int i= arr.length-1;i>=0;i--)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+	
+	
+}
