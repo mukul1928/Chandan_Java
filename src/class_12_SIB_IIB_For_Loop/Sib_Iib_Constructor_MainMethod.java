f9c133536ecdada280550d0464b07af25557475a
Chandan Pandey <chandanpandey1928@gmail.com>
2025-07-14 13:07:16 +0530

    Update Sib_Iib_Constructor_MainMethod.java

--- src/class_12_SIB_IIB_For_Loop/Sib_Iib_Constructor_MainMethod.java
+++ src/class_12_SIB_IIB_For_Loop/Sib_Iib_Constructor_MainMethod.java
@@ -2,7 +2,7 @@ package class_12_SIB_IIB_For_Loop;
 
 public class Sib_Iib_Constructor_MainMethod 
 {
-	static 
+	static // This is the SIB
 	{
 		System.out.println("SIB");
 	}
@@ -10,10 +10,10 @@ public class Sib_Iib_Constructor_MainMethod
 	{
 		System.out.println("Constructor");
 	}
-	{
+	{ // This block is called the IIB the opening and the closing of this block
 		System.out.println("IIB");
 	}
-	public static void main(String[] args) 
+	public static void main(String[] args) // This is the main method 
 	{
 		System.out.println("Main Method");
 		new Sib_Iib_Constructor_MainMethod();
