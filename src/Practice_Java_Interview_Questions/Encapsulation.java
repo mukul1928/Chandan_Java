d3c8db4a265b8373ee52cd61ec8394c2e0d8ed21
Chandan Pandey <chandanpandey1928@gmail.com>
2025-08-14 10:51:21 +0530

    Update Encapsulation.java

--- src/Practice_Java_Interview_Questions/Encapsulation.java
+++ src/Practice_Java_Interview_Questions/Encapsulation.java
@@ -2,7 +2,7 @@ package Practice_Java_Interview_Questions;
 class Sensititive_Info
 {
 	private String un = "chandanpandey90";
-	private String pas = "smitalovechandan143";
+	private String pas = "chandan143";
 	
 	public String getUn() {
 		return un;
@@ -21,9 +21,9 @@ public class Encapsulation {
 	public static void main(String[] args) 
 	{
 		Sensititive_Info s = new Sensititive_Info();
-		s.setUn("mukullovesmita143@hotmail.com");
+		s.setUn("mukul143@hotmail.com");
 		System.out.println(s.getUn());
-		s.setPas("smitalovechandan@143mylove");
+		s.setPas("chandan@143");
 		System.out.println(s.getPas());
 	}
 }
