7c106ffbafe94df699629c8af158e814b4becadf
Chandan Pandey <chandanpandey1928@gmail.com>
2025-09-16 11:47:15 +0530

    Java Program To Implement HashCode And Equals

---
+++ src/Top_SDET_InterviewQuestions_And_Answers/CountOddAndEvenNumberFromGivenArray.java
@@ -0,0 +1,24 @@
+package Top_SDET_InterviewQuestions_And_Answers;
+
+public class CountOddAndEvenNumberFromGivenArray {
+	// input: {1,2,3,4,5,6,7,8,9}
+	public static void main(String[] args) {
+		int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		int[] count = countOddAndEven(array);
+		System.out.println("Even numbers count: " + count[1]);
+		System.out.println("Odd numbers count: " + count[0]);
+	}
+
+	public static int[] countOddAndEven(int[] array) {
+		int[] count = new int[2]; // Index 0 for odd count, Index 1 for even count
+		for (int num : array) {
+			if (num % 2 == 0) {
+				count[1]++; // Increment even count
+			} else {
+				count[0]++; // Increment odd count
+			}
+		}
+		return count;
+	}
+
+}
